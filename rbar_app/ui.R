8ccf9a218e544cdd27aac5fe8d7315822d4aa3a3
ian <ian.wells@rationalsystems.com>
2015-02-11 21:38:54 -0600

    more reasonable default time range

--- rbar_app/ui.R
+++ rbar_app/ui.R
@@ -2,7 +2,7 @@ library(shiny)
 
 demoevents = c('OTC Week','FPSF','Rodeo','Baseball Season','Football Season','Basketball Season','Baseball Season','Hurricane Ike','Hurricane Rita','Hurricane Katrina','NBA All Star Game','MLB All Star Game','Superbowl XXXVIII','Halloween','New Years','Spring Break','School Year','Light Rail Opens','Restaurant Week','Pride Parade','Holidays')
 
-b = read.csv('./bars.csv')[,1]
+b = read.csv('./bars.csv')[,'name']
 
 shinyUI(fluidPage(
 
@@ -14,7 +14,7 @@ shinyUI(fluidPage(
                 b, width = '100%'),
     selectInput("events", "Select Events:",multiple = TRUE,
                 demoevents, width = '100%'),
-    dateRangeInput("daterange", label = "Date range:", start = as.Date('2001-01-01'), end = as.Date('2015-01-01'), min = as.Date('2001-01-01'), max = as.Date('2016-01-01'), format = "yyyy-mm-dd", startview = "month"),
+    dateRangeInput("daterange", label = "Date range:", start = as.Date('2003-01-01'), end = as.Date('2015-01-01'), min = as.Date('2001-01-01'), max = as.Date('2016-01-01'), format = "yyyy-mm-dd", startview = "month"),
     radioButtons("style", label = "Style",
                  choices = list("Lines" = 1, "Points" = 2), 
                  selected = 1)
